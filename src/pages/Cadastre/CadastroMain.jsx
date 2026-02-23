d6354d1eee2e05c32b87fcb41391b6acf27ef69e
emerson <emerson@avipet.rede>
2026-02-23 11:32:10 -0300

    Versão 1.0 cadastro finalizado

--- src/pages/Cadastre/CadastroMain.jsx
+++ src/pages/Cadastre/CadastroMain.jsx
@@ -6,36 +6,40 @@ function CadastroMain() {
     return (
         <main className="home">
             <div className="cadastre-container " >
-                <h1 className="home-title">Cadastro</h1>
-                <form action="" className="cadastre-form ">
+                <div className="cadastre-layout ">
+                    <h1 className="home-title">Cadastro de Usuário</h1>
+                    <form action="" className="cadastre-form ">
 
-                    <section  >
-                        <label htmlFor="nome"></label>
-                        <input type="text" name="nome" id="nome" placeholder="Nome Completo" />
-                    </section>
+                        <section className="input-group">
+                            <FaUser className="icon" />
+                            <label htmlFor="nome"></label>
+                            <input type="text" name="nome" id="nome" placeholder="Nome Completo" />
+                        </section>
 
-                    <section >
-                        <label htmlFor="email"></label>
-                        <input type="email" name="email" id="email" placeholder="Email" />
-                    </section>
+                        <section className="input-group">
+                            <FaEnvelope className="icon" />
+                            <label htmlFor="email"></label>
+                            <input type="email" name="email" id="email" placeholder="Email" />
+                        </section>
 
-                    <section >
-                        <label htmlFor="senha"></label>
-                        <input type="password" name="senha" id="senha" placeholder="Senha" />
-                    </section>
+                        <section className="input-group">
+                            <FaLock className="icon" />
+                            <label htmlFor="senha"></label>
+                            <input type="password" name="senha" id="senha" placeholder="Senha" />
+                        </section>
 
-                    <section >
-                        <label htmlFor="ConfirmarSenha"></label>
-                        <input type="password" name="ConfirmarSenha" id="ConfirmarSenha" placeholder="Senha" />
-                    </section>
-                </form>
-                <button className="cadastre-button">Cadastrar</button>
-                <div className="cadastre-options">
-                    <div>Esqueceu a senha?</div>
-                    <div>Já possui cadastro?Login</div>
+                        <section className="input-group">
+                            <FaLock className="icon" />
+                            <label htmlFor="ConfirmarSenha"></label>
+                            <input type="password" name="ConfirmarSenha" id="ConfirmarSenha" placeholder="Confirme a Senha" />
+                        </section>
+                    </form>
+                    <button className="cadastre-button">Cadastrar</button>
+                    <div className="cadastre-options">
+                        <div>Já possui cadastro?Login</div>
+                    </div>
                 </div>
             </div>
-
         </main>
     )
 }
