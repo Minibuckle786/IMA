d8bbf05689ee7032aaae15672c42d411089d6e65
Emerson de Oliveira <emerson_yeda@hotmail.com>
2026-02-24 22:03:13 -0300

    Painel admin

--- src/pages/Dashboard/DashboardMain.jsx
+++ src/pages/Dashboard/DashboardMain.jsx
@@ -1,7 +1,19 @@
+import "./DashboardMain.css"
+
 function DashboardMain() {
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="DashboardMain">
+            <div className="DashboardMain-Layout">
+                <div className="DashboardMain-h1">
+                    <h1>Meu Painel</h1>
+                </div>
+                <div>
+                    <button className="DashboardMain-button1">Novo Painel</button>
+                </div>
+
+            </div>
+            <hr />
+            <button className="DashboardMain-button2">Novo Painel</button>
         </div>
     )
 }
