52e1cb417856ed8d62efeddb8d0e471dc64b2a95
emerson <emerson@avipet.rede>
2026-02-26 15:15:26 -0300

    Version 1.0 Ajustes Botões das triagens admin

--- src/pages/ServicosPorAssunto/ServicosPorAssunto.jsx
+++ src/pages/ServicosPorAssunto/ServicosPorAssunto.jsx
@@ -2,57 +2,54 @@ import "./ServicosPorAssunto.css"
 
 function ServicosPorAssunto() {
     return (
-        <div>
-            <div>
-                <section className="cards-layout">
-                    <h2>Serviços por assunto</h2>
-                    <ul class="cards">
-                        <li>
-                            <span>Água, esgoto e meio ambiente</span>
-                            <div className="cards-opcao">
-                                <button>Abrir</button>
-                                <button>Editar</button>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Atendimento a consumidores</span>
-                            <div className="cards-opcao">
-                                <button>Abrir</button>
-                                <button>Editar</button>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Água, esgoto e meio ambiente</span>
-                            <div className="cards-opcao">
-                                <button>Abrir</button>
-                                <button>Editar</button>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Atendimento a consumidores</span>
-                            <div className="cards-opcao">
-                                <button>Abrir</button>
-                                <button>Editar</button>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Água, esgoto e meio ambiente</span>
-                            <div className="cards-opcao">
-                                <button>Abrir</button>
-                                <button>Editar</button>
-                            </div>
-                        </li>
-                        <li>
-                            <span>Atendimento a consumidores</span>
-                            <div className="cards-opcao">
-                                <button>Abrir</button>
-                                <button>Editar</button>
-                            </div>
-                        </li>
-                    </ul>
-                </section>
-            </div>
-        </div>
+
+        <section className="cards-layout">
+            <h2>Serviços por assunto</h2>
+            <ul class="cards">
+                <li>
+                    <span>Água, esgoto e meio ambiente</span>
+                    <div className="cards-opcao">
+                        <button>Abrir</button>
+                        <button>Editar</button>
+                    </div>
+                </li>
+                <li>
+                    <span>Atendimento a consumidores</span>
+                    <div className="cards-opcao">
+                        <button>Abrir</button>
+                        <button>Editar</button>
+                    </div>
+                </li>
+                <li>
+                    <span>Água, esgoto e meio ambiente</span>
+                    <div className="cards-opcao">
+                        <button>Abrir</button>
+                        <button>Editar</button>
+                    </div>
+                </li>
+                <li>
+                    <span>Atendimento a consumidores</span>
+                    <div className="cards-opcao">
+                        <button>Abrir</button>
+                        <button>Editar</button>
+                    </div>
+                </li>
+                <li>
+                    <span>Água, esgoto e meio ambiente</span>
+                    <div className="cards-opcao">
+                        <button>Abrir</button>
+                        <button>Editar</button>
+                    </div>
+                </li>
+                <li>
+                    <span>Atendimento a consumidores</span>
+                    <div className="cards-opcao">
+                        <button>Abrir</button>
+                        <button>Editar</button>
+                    </div>
+                </li>
+            </ul>
+        </section>
     )
 }
 
