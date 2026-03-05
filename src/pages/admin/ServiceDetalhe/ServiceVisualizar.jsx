e7ebdf8bbf609a0c222f749180d028f790c0c5d8
emerson <emerson@avipet.rede>
2026-03-05 17:16:03 -0300

    Categoria Detalhes parte4

--- src/pages/admin/ServiceDetalhe/ServiceVisualizar.jsx
+++ src/pages/admin/ServiceDetalhe/ServiceVisualizar.jsx
@@ -5,13 +5,45 @@ function ServiceVisualizar() {
     return (
         <>
             <div className="servicevisualizar-layout">
-                <div>
+                <div className="header-visualizar">
                     <p>Voltar para triagens</p>
-
+                    <h2>Visualizando: <span>Segunda via Sanasa</span> </h2>
+                    <p>Modo simulação</p>
                 </div>
+                <div className="body-visualizar">
+                    <div className="bodyLayout-visualizar">
+                        <div className="card-pergunta">
+                            <div className="card-header">
+                                <h3>Pergunta 1 de 3</h3>
+                            </div>
 
+                            <hr />
+
+                            <div className="card-body">
+                                <p >Você tem o número da matrícula?</p>
+                                <div className="pergunta">
+                                    <label className="opcao">
+                                        <input type="radio" name="matricula" value="sim" />
+                                        Sim
+                                    </label>
+                                    <label className="opcao">
+                                        <input type="radio" name="matricula" value="nao" />
+                                        Não
+                                    </label>
+                                </div>
+                            </div>
+
+                            <hr />
+
+                            <div class="card-footer">
+                                <button class="btn-proxima">Próxima</button>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
             </div>
-
         </>
     )
 }
